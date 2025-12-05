54dc57e685f54bbff9b7af4c4d0cb541afca4780
Alan <11330024@dcsh.tp.edu.tw>
2025-12-05 11:45:53 +0800

    完成投票主題列表、查看投票主題、投票功能

--- default/views.py
+++ default/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Poll, Option
+from django.views.generic import ListView, DetailView, RedirectView
+from django.urls import reverse
 
 # Create your views here.
+def poll_list(req):
+    polls = Poll.objects.all()
+    return render(req, "default/list.html", {'poll_list': polls, 'msg':  'Hello!'})
+
+class Pollist(ListView):
+    model =Poll
+
+
+class PollView (DetailView):
+    model = Poll
+
+    def get_context_data(self, **kwargs):
+        ctx =super().get_context_data(**kwargs)
+        ctx['option_list'] = Option.objects.filter(poll_id=self.object.id)
+        return ctx
+    
+class PollVote(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        option = Option.objects.get(id = self.kwargs['oid'])
+        option.votes += 1 #option.votes = options.votes + 1
+        option.save()
+        #return "/poll/{}/".format(option.poll_id)
+        #return f"/poll/{option.poll_id}"
+        #return reverse('poll_view', args=[option.poll_id])
+        return reverse('poll_view', kwargs={'pk': option.poll_id})
